ee8887cffd90dd5c8df3a8425d2f11ed93e93489
Jordan Lee <jordan.lee@alintaenergy.com.au>
2020-09-22 15:13:19 +1000

    Updates

---
+++ precache-manifest.a4fa37ee36dd270838f8a405b9574ea6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b609ffd830897d74cb950e549440f80",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "95323dd483f051799e48",
+    "url": "/monsters/static/css/main.3b219a3c.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/monsters/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95323dd483f051799e48",
+    "url": "/monsters/static/js/main.415acfdf.chunk.js"
+  },
+  {
+    "revision": "55328b6a8e96452d341b",
+    "url": "/monsters/static/js/runtime-main.3a250f91.js"
+  }
+]);
